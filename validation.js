7383614372b89dad4fdfcf8058bb06cb8206385e
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-05-10 10:00:07 +0200

    Create validation.js

---
+++ validation.js
@@ -0,0 +1,64 @@
+function isNotEmpty(value) {
+  return value.trim() !== "";
+}
+
+function isEmail(value) {
+  return value.
+// contiene @
+}
+
+function isPhone(value)¨{
+  // tiene que tener 9 digitos
+}
+function isURL(value)¨{
+  // tiene que tener 
+}
+function isPassword(value)¨{
+
+}
+function isCreditCard(value)¨{
+
+}
+ 
+ 
+function isInteger(value)¨{
+
+}
+function isPositive(value)¨{
+
+}
+function isNegative(value)¨{
+
+}
+function isWithinRange(value, min, max) {
+
+}
+ 
+function isDate(value)¨{
+
+}
+function isbefore(value)¨{
+
+}
+function isAfter(value)¨{
+
+}
+ 
+function isEmpty(value)¨{
+
+}
+function isequal(value)¨{
+
+}
+function isDifferent(value)¨{
+
+}
+
+
+
+
+
+
+module.exports = {
+  isNotEmpty,
+}
